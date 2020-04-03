b8a4aefb18d25cbb23cfd3966e4714068b9d4efd
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-03 10:58:58 -0400

    wrapping components in other components using props.children

--- components/src/ApprovalCard.js
+++ components/src/ApprovalCard.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
-const ApprovalCard = () => {
+const ApprovalCard = (props) => {
+	// we wrapped a component in another component and references it with props
+	// all wrapped components come in under props.children 
 	return (
 		<div className="ui card">
-			<div className="comment">
-			Are you sure?
+			<div className="content">
+			{props.children}
 			</div>
 			<div className="extra content">
 				<div className="ui two buttons">
--- components/src/index.js
+++ components/src/index.js
@@ -9,25 +9,34 @@ const App = () => {
 	// Components are the exception to the JS rule in JSX
 	return (
 		<div className="ui container comments">
-			<ApprovalCard />
-			<CommentDetail
-			author="Sam" 
-			timeAgo="Today at 4:45PM" 
-			comment="nice comments" 
-			avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-			author="Alex"
-			timeAgo="Today at 2:00AM"
-			comment="woot a comment"
-			avatar={faker.image.avatar()}
-			/>
-			<CommentDetail
-			author="Jane"
-			timeAgo="Yesterday at 5:00PM"
-			comment="1337"
-			avatar={faker.image.avatar()}
-			/>
+			<ApprovalCard>
+				<h4>Warning</h4>
+				Are you sure you want to do this?
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+				author="Sam" 
+				timeAgo="Today at 4:45PM" 
+				comment="nice comments" 
+				avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+				author="Alex"
+				timeAgo="Today at 2:00AM"
+				comment="woot a comment"
+				avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
+			<ApprovalCard>
+				<CommentDetail
+				author="Jane"
+				timeAgo="Yesterday at 5:00PM"
+				comment="1337"
+				avatar={faker.image.avatar()}
+				/>
+			</ApprovalCard>
 		</div>
 	)
 }
