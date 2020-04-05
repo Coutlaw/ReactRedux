a179b0f9928e5f73d1f1252d3102c86c69423638
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-05 12:53:12 -0400

    refactored App to a class based component that inherits from React.Component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,17 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-	// getCurrentPositions has two callbacks: a success and failure
-	window.navigator.geolocation.getCurrentPosition(
-		// success callback
-		(position) => console.log(position),
-		// failure callback
-		(err) => console.log(err)
-	);
-	return(
-		<div>Hello World!</div>
-	)
+// const App = () => {
+// 	// getCurrentPositions has two callbacks: a success and failure
+// 	window.navigator.geolocation.getCurrentPosition(
+// 		// success callback
+// 		(position) => console.log(position),
+// 		// failure callback
+// 		(err) => console.log(err)
+// 	);
+// 	return(
+// 		<div>Latitude</div>
+// 	)
+// }
+
+class App extends React.Component {
+	render(){
+		// getCurrentPositions has two callbacks: a success and failure
+			window.navigator.geolocation.getCurrentPosition(
+				// success callback
+				(position) => console.log(position),
+				// failure callback
+				(err) => console.log(err)
+			);
+		return <div>Latitude: </div>
+	}
 }
 
 ReactDOM.render(
