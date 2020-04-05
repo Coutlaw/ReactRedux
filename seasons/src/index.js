5e8be4639e7b6c9ccee045660b1f826eb3a1a65b
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-05 12:54:13 -0400

    removed dead code from functional component

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,19 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// const App = () => {
-// 	// getCurrentPositions has two callbacks: a success and failure
-// 	window.navigator.geolocation.getCurrentPosition(
-// 		// success callback
-// 		(position) => console.log(position),
-// 		// failure callback
-// 		(err) => console.log(err)
-// 	);
-// 	return(
-// 		<div>Latitude</div>
-// 	)
-// }
-
 class App extends React.Component {
 	render(){
 		// getCurrentPositions has two callbacks: a success and failure
