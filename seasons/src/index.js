c13d96a7caf6149c0fcdbee0b3d926c98e7ea5b3
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-06 08:18:47 -0400

    created a constructor for a react componnent, and initilized state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -2,8 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 class App extends React.Component {
+	// component constructor which is ran on initialization
+	constructor(props) {
+		// superclass constructor
+		super(props);
+
+		// state object: object with lat property (null because it has no value)
+		this.state = { lat: null };
+	}
+	
+	// required for every React.Component
 	render(){
-		// getCurrentPositions has two callbacks: a success and failure
+			// getCurrentPositions has two callbacks: a success and failure
 			window.navigator.geolocation.getCurrentPosition(
 				// success callback
 				(position) => console.log(position),
