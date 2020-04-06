5e5beb9859dee7e1e7d37d5c68686f12fe00db57
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-06 10:05:47 -0400

    updated error handling callback

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -8,19 +8,35 @@ class App extends React.Component {
 		super(props);
 
 		// state object: object with lat property (null because it has no value)
-		this.state = { lat: null };
+		this.state = { lat: null, errorMessage: '' };
+
+		// getCurrentPositions has two callbacks: a success and failure
+		window.navigator.geolocation.getCurrentPosition(
+			// success callback
+			(position) => {
+				// Updates our state object which will re-trigger the render function
+				this.setState({ lat: position.coords.latitude });
+
+				//WE NEVER WANT TO DO THIS
+				//this.state.lat = position.coords.latitude
+				// we only do direct assignments on initialization
+			},
+			// failure callback
+			(err) => {
+				this.setState({ errorMessage: err.message});
+			}
+		);
 	}
 	
 	// required for every React.Component
-	render(){
-			// getCurrentPositions has two callbacks: a success and failure
-			window.navigator.geolocation.getCurrentPosition(
-				// success callback
-				(position) => console.log(position),
-				// failure callback
-				(err) => console.log(err)
-			);
-		return <div>Latitude: </div>
+	render() {
+		return (
+		<div>
+			Latitude: {this.state.lat}
+			<br />
+			Error: {this.state.errorMessage}
+		</div>
+		);
 	}
 }
 
