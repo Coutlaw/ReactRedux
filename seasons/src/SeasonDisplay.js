053ac5a9270d9172a3b0bf2f846eeed4fe9be9d5
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-06 12:14:57 -0400

    refactor to remve expressions and turnarys

--- seasons/src/SeasonDisplay.js
+++ seasons/src/SeasonDisplay.js
@@ -1,17 +1,20 @@
 import React from 'react';
 
+const seasonConfig = {
+	summer: { text: 'Lets hit the beach', icon: 'sun' },
+	winter: { text: 'Burr, it is chilly', icon: 'snowflake' }
+};
+
 const getSeason = (lat, month) => {
 	 if(month > 2 && month < 9) {
-		 return lat > 0 ? 'summer' : 'winter';
+		 return seasonConfig.summer;
 	 } else {
-		return lat > 0 ? 'winter' : 'summer';
+		return seasonConfig.winter;
 	 }
 }
 
 const SeasonDisplay = (props) => {
-	const season = getSeason(props.lat, new Date().getMonth());
-	const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach'
-	const icon = season === 'winter' ? 'snowflake' : 'sun'
+	const { text, icon } = getSeason(props.lat, new Date().getMonth());
 
 	return (
 	<div>
