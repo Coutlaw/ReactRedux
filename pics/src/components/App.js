37be526e536970a0b1d007cdb4f387a421f9bc33
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-07 14:03:20 -0400

    refactored to arrow function and updating state in App

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,9 +3,11 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component{
+	state = { images: [] }
+
 	// callback function
-	onSearchSubmit(term) {
-		axios.get('https://api.unsplash.com/search/photos',
+	onSearchSubmit = async (term) => {
+		const response = await axios.get('https://api.unsplash.com/search/photos',
 		{
 			params: {
 				query: term
@@ -14,12 +16,20 @@ class App extends React.Component{
 				Authorization: 'Client-ID l7dcR04kK52Au8y_ozrIu3pSN7V1RFkknX61Wi_ee78'
 			}
 		});
+
+		this.setState({ images: response.data.results });
+
+		// 👇 this is the pre async await way of doing things
+		// .then((response) => {
+		// 	console.log(response.data.results);
+		// });
 	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}> 
 				<SearchBar onSubmit={this.onSearchSubmit}/>
+				Found: {this.state.images.length} images.
 			</div>
 		);
 	}
