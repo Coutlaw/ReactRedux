76099f0d7bde76e73c1ba08987efa7d67c7ce4b1
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-07 13:48:54 -0400

    using axios/ajax to make a request to an external API

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -5,7 +5,15 @@ import SearchBar from './SearchBar';
 class App extends React.Component{
 	// callback function
 	onSearchSubmit(term) {
-		console.log(term);
+		axios.get('https://api.unsplash.com/search/photos',
+		{
+			params: {
+				query: term
+			},
+			headers: {
+				Authorization: 'Client-ID l7dcR04kK52Au8y_ozrIu3pSN7V1RFkknX61Wi_ee78'
+			}
+		});
 	}
 
 	render() {
