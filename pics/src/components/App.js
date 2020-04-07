13b72e29f700a06cc583aca61c62548acbbf2bc7
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-07 14:22:50 -0400

    added an image list to render all images returned as a list, with mapping and list indexing for performance

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,35 +1,25 @@
 import React from 'react';
-import axios from 'axios';
 import SearchBar from './SearchBar';
+import ImaageList from './ImageList'
+import unsplash from '../api/unsplash';
+import ImageList from './ImageList';
 
 class App extends React.Component{
 	state = { images: [] }
 
 	// callback function
 	onSearchSubmit = async (term) => {
-		const response = await axios.get('https://api.unsplash.com/search/photos',
-		{
-			params: {
-				query: term
-			},
-			headers: {
-				Authorization: 'Client-ID l7dcR04kK52Au8y_ozrIu3pSN7V1RFkknX61Wi_ee78'
-			}
-		});
+		const response = await unsplash.get('/search/photos',
+		{ params: { query: term } });
 
 		this.setState({ images: response.data.results });
-
-		// 👇 this is the pre async await way of doing things
-		// .then((response) => {
-		// 	console.log(response.data.results);
-		// });
 	}
 
 	render() {
 		return (
 			<div className="ui container" style={{ marginTop: '10px' }}> 
 				<SearchBar onSubmit={this.onSearchSubmit}/>
-				Found: {this.state.images.length} images.
+				<ImageList images={this.state.images} />
 			</div>
 		);
 	}
