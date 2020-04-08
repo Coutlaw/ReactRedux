02d860d0db775c1ce3b32652931f2afec5bcb1c0
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-08 15:08:28 -0400

    redux being used to tie into props

--- songs/src/components/SongList.js
+++ songs/src/components/SongList.js
@@ -1,9 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 class SongList extends Component {
 	render() {
+		// proof that props is now redux state
+		//console.log(this.props.songs);
 		return <div>SongList</div>; 
 	}
 }
 
-export default SongList;
+// this will show up as props to the containing component 👆
+const mapStateToProps = (state) => {
+
+	console.log(state);
+	return {
+		songs: state.songs
+	};
+}
+
+// connects redux to the component
+export default connect(mapStateToProps) (SongList);
