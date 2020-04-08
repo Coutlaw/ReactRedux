8c6fd8e3b2f3beed342002b7dc3a8462e3e9db6b
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-08 14:26:13 -0400

    created a song list component that will manage my song list

--- songs/src/index.js
+++ songs/src/index.js
@@ -3,6 +3,7 @@ import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import reducers from './reducers';
 
 ReactDOM.render(
   <Provider store={createStore(reducers)}>
