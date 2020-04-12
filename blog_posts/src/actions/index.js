23da22d3c2d7fd18af0b7b2ebe982cb75788ff5b
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-12 08:41:46 -0400

    new component, action creator, and reducer for retriving user data

--- blog_posts/src/actions/index.js
+++ blog_posts/src/actions/index.js
@@ -7,3 +7,12 @@ export const fetchPosts = () => {
 		dispatch({ type: 'FETCH_POSTS', payload: response.data });
 	};
 };
+
+// fetch single user
+export const fetchUser = (id) => {
+	return async (dispatch) => {
+		const response = await jsonPlaceholder.get(`/users/${id}`);
+
+		dispatch({ type: 'FETCH_USER', payload: response.data });
+	};
+};
---
+++ blog_posts/src/components/UserHeader.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { fetchUser } from '../actions';
+
+class UserHeader extends React.Component {
+	componentDidMount() {
+		this.props.fetchUser(this.props.userId);
+	}
+	
+	render() {
+		const { user } = this.props;
+
+		if(!user) {
+			return null;
+		}
+
+		return (
+		<div className="header">{user.name}</div>
+		);
+	}
+};
+
+// ownProps is a reference to the props sent into the calling component
+const mapStateToProps = (state, ownProps) => {
+	return { user: state.users.find(user => user.id === ownProps.userId) };
+};
+
+export default connect(mapStateToProps, { fetchUser }) (UserHeader);
