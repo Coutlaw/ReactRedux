41177d0b3a342f6387796cb149a8ce3a6d5d59b5
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-14 08:31:59 -0400

    created an always showing header component

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,10 +1,11 @@
-import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-import StreamCreate from "./streams/StreamCreate";
-import StreamEdit from "./streams/StreamEdit";
-import StreamDelete from "./streams/StreamDelete";
-import StreamList from "./streams/StreamList";
-import StreamShow from "./streams/StreamShow";
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 // const PageOne = () => {
 //   return (
@@ -27,8 +28,9 @@ import StreamShow from "./streams/StreamShow";
 
 const App = () => {
   return (
-    <div>
+    <div className="ui container">
       <BrowserRouter>
+        <Header />
         <div>
           <Route path="/" exact component={StreamList} />
           <Route path="/streams/new" exact component={StreamCreate} />
