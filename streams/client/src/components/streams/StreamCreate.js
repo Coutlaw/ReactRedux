ab073fabf1e18ac76caf816113cfe783dac7960e
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-17 11:15:16 -0400

    added form handling with redux-form

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
 
-const StreamCreate = () => {
-  return <div>StreamCreate</div>;
-};
+class StreamCreate extends React.Component {
+  // reduxForm will create the props to use these methods from component
+  // destructure input and label from the props
+  renderInput({ input, label }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+      </div>
+    );
+  }
 
-export default StreamCreate;
+  render() {
+    return (
+      <form className="ui form">
+        <Field name="title" component={this.renderInput} label="Enter Title"/>
+        <Field name="description" component={this.renderInput} label="Enter Label"/>
+      </form>
+    );
+  }
+}
+
+export default reduxForm({
+  form: 'StreamCreate',
+})(StreamCreate);
