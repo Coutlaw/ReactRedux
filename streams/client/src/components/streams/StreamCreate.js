b6d0b141c6aaa1de9fe664e2ad6ef8b13bb398af
Cass Outlaw <cass.d.outlaw@gmail.com>
2020-04-17 12:11:57 -0400

    refactoring errors

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -4,14 +4,26 @@ import { Field, reduxForm } from 'redux-form';
 class StreamCreate extends React.Component {
   // reduxForm will create the props to use these methods from component
   // destructure input and label from the props
-  renderInput({ input, label, meta }) {
+  renderInput = ({ input, label, meta }) => {
+    const className = `field ${meta.error && meta.touched ? 'error' : ''}`;
+
     return (
-      <div className="field">
+      <div className={className}>
         <label>{label}</label>
         <input {...input} />
-        <label>{meta.error}</label>
+        {this.renderError(meta)}
       </div>
     );
+  };
+
+  renderError({ error, touched }) {
+    if (touched && error) {
+      return (
+        <div className="ui error message">
+          <div className="header">{error}</div>
+        </div>
+      );
+    }
   }
 
   // gets called as a callback from handleSubmit
@@ -25,7 +37,7 @@ class StreamCreate extends React.Component {
     return (
       <form
         onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form"
+        className="ui form error"
       >
         <Field name="title" component={this.renderInput} label="Enter Title" />
         <Field
